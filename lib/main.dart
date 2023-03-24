8d3d3599e8e1032f5026d26e4a5143859999839b
Sachal Raja <sachal.raja@gmail.com>
2023-03-25 00:38:48 +0500

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,328 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(Calculator_k191362());
+  runApp(const MyApp());
 }
 
-class Calculator_k191362 extends StatelessWidget {
-  //const Calculator_k191362({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title:'Scientific Calculator',
-      theme: ThemeData(primarySwatch: Colors.lightGreen),
-      home: SimpleCalculator(),
+      title: 'Settings',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.robotoTextTheme(),
+      ),
+      home: const SettingsScreen(),
     );
   }
 }
 
-class SimpleCalculator extends StatefulWidget {
-  //const SimpleCalculator({Key? key}) : super(key: key);
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
 
-  @override
-  State<SimpleCalculator> createState() => _SimpleCalculatorState();
-}
-
-class _SimpleCalculatorState extends State<SimpleCalculator> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Simple Calculator')),
-      body: Column(
-        children: <Widget>[
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: const Text(
+                'Connections',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Card(
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: const Icon(Icons.wifi),
+                    title: const Text('Wi-Fi'),
+                    subtitle: const Text('Connected to home network'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {},
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(Icons.bluetooth),
+                    title: const Text('Bluetooth'),
+                    subtitle: const Text('On'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {},
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(Icons.nfc),
+                    title: const Text('NFC and payment'),
+                    subtitle: const Text('On'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {},
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(Icons.airplanemode_on),
+                    title: const Text('Airplane mode'),
+                    subtitle: const Text('Off'),
+                    trailing: Switch(
+                      value: false,
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: const Text(
+                'Device',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Card(
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: const Icon(Icons.battery_full),
+                    title: const Text('Battery'),
+                    subtitle: const Text('74%'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {},
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(Icons.storage),
+                    title: const Text('Device care'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {},
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(Icons.security),
+                    title: const Text('Lock screen'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LockScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(Icons.developer_mode),
+                    title: const Text('Developer options'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: const Text(
+                'Display',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Card(
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: const Icon(Icons.brightness_medium),
+                    title: const Text('Brightness'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {},
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(Icons.wallpaper),
+                    title: const Text('Wallpaper'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {},
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(Icons.screen_lock_portrait),
+                    title: const Text('Screen rotation'),
+                    trailing: Switch(
+                      value: false,
+                      onChanged: (value) {},
+                    ),
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(Icons.font_download),
+                    title: const Text('Font and screen zoom'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: const Text(
+                'Sounds and vibration',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Card(
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: const Icon(Icons.volume_up),
+                    title: const Text('Volume'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {},
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(Icons.do_not_disturb_on),
+                    title: const Text('Do not disturb'),
+                    subtitle: const Text('Until next alarm'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {},
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(Icons.vibration),
+                    title: const Text('Vibration'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {},
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(Icons.south_east_rounded),
+                    title: const Text('Sound quality and effects'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: const Text(
+                'Notifications',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Card(
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: const Icon(Icons.notifications),
+                    title: const Text('App notifications'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {},
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(Icons.message),
+                    title: const Text('Messages'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {},
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(Icons.email),
+                    title: const Text('Email'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: const Text(
+                'Privacy and security',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Card(
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: const Icon(Icons.fingerprint),
+                    title: const Text('Fingerprint scanner'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {},
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(Icons.security),
+                    title: const Text('Google Play Protect'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {},
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(Icons.location_on),
+                    title: const Text('Location'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LocationScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-          Container(
-            alignment: Alignment.centerRight,
-            padding: EdgeInsets.fromLTRB(10, 20, 20, 0),
-            child: Text("0", style: TextStyle(fontSize: 38.0),),
-          ),
+class LockScreen extends StatelessWidget {
+  const LockScreen({super.key});
 
-          Container(
-            alignment: Alignment.centerRight,
-            padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
-            child: Text("0", style: TextStyle(fontSize: 48.0),),
-          ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lock Screen'),
+      ),
+      body: const Center(
+        child: Text('This is the Lock Screen'),
+      ),
+    );
+  }
+}
 
-          Expanded(
-            child: Divider(),
-          ),
+class LocationScreen extends StatelessWidget {
+  const LocationScreen({super.key});
 
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:<Widget>[
-              Container(
-                width:MediaQuery.of(context).size.width*.75,
-              )
-            ],
-          )
-        ],
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Location'),
+      ),
+      body: const Center(
+        child: Text('This is the Location Screen'),
       ),
     );
   }
